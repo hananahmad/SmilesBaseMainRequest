c3e2447c503e53db23b64bdc7e447cd1aa7dea7b
Hanan Ahmed <hanancomsian19@gmail.com>
2023-02-24 10:33:48 +0400

    Change in base main request to make alignment

--- Sources/SmilesBaseMainRequestManager/BaseMainResponseAdditionalInfo.swift
+++ Sources/SmilesBaseMainRequestManager/BaseMainResponseAdditionalInfo.swift
@@ -9,14 +9,13 @@ import Foundation
 
 public class BaseMainResponseAdditionalInfo : Codable {
     
-    @objc var name : String?
-    @objc var value : String?
+    @objc public var name : String?
+    @objc public var value : String?
     
     enum CodingKeys: String, CodingKey {
         case name = "name"
         case value = "value"
     }
-    
     
     required public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
